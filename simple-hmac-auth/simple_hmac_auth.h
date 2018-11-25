2e5a929409daf5283397bbfbf7381eb8f8afe7b4
Jesse <jessetyoungblood@gmail.com>
2018-11-24 21:20:57 -0500

    Initial Commit

---
+++ simple-hmac-auth/simple_hmac_auth.h
@@ -0,0 +1,19 @@
+//
+//  simple_hmac_auth.h
+//  simple-hmac-auth
+//
+//  Created by Jesse T Youngblood on 11/24/18.
+//  Copyright © 2018 Jesse T Youngblood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for simple_hmac_auth.
+FOUNDATION_EXPORT double simple_hmac_authVersionNumber;
+
+//! Project version string for simple_hmac_auth.
+FOUNDATION_EXPORT const unsigned char simple_hmac_authVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <simple_hmac_auth/PublicHeader.h>
+
+
